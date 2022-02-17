4f96d3ee08ba35fb5ce93f85ab85abc89cf93d54
Nick M <mully7773@gmail.com>
2022-02-16 19:00:05 -0600

    Added webpacks and standard rules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,33 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+
       
+
+      //TODO: Add Inject and WebpackManifest Tomorrow
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
